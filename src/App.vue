993f6c89cc81ae3ed8122b180b4d3ebc27b0430b
kaixingg <1536863186@qq.com>
2022-07-29 12:45:15 +0800

    封装网络请求

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <div id="app">跟组件</div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import { request } from '@/network'
+export default {
+  name: 'App',
+  props: {},
+  data () {
+    return {
     }
-  }
+  },
+  // 这时可以访问data和methods
+  created () {
+    this.getList()
+  },
+  // 方法
+  methods: {
+    async getList () {
+      const res = await request({
+        url: 'http://geek.itheima.net/v1_0/channels'
+      })
+      console.log(res)
+    }
+  },
+  // 计算属性
+  computed: {},
+  // 组件注册
+  components: {},
 }
+</script>
+
+<style scoped lang="less">
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/assets/css/global.less' //全局初始化样式
+import '@/elementUI'  //注册element-UI 组件库
 
 Vue.config.productionTip = false
 
